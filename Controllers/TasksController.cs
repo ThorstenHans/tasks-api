30b8b4b8ca2252db9c895953aea0e45ce4b75f03
Thorsten Hans <thorsten.hans@gmail.com>
2018-07-05 20:41:46 +0200

    chore(proj): reformat controller

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -6,60 +6,49 @@ using Microsoft.AspNetCore.Mvc;
 using Tasks.API.Repositories;
 using Models = Tasks.API.Models;
 
-namespace Tasks.API.Controllers
-{
-    [Route("api/[controller]")]
-    public class TasksController : Controller
-    {
+namespace Tasks.API.Controllers {
+    [Route ("api/[controller]")]
+    public class TasksController : Controller {
         private ITasksRepository Repository { get; }
 
-        public TasksController(ITasksRepository repository)
-        {
+        public TasksController (ITasksRepository repository) {
             Repository = repository;
         }
+
         [HttpGet]
-        public IEnumerable<Models.Task> Get()
-        {
-            return Repository.GetAllTasks();
+        public IEnumerable<Models.Task> Get () {
+            return Repository.GetAllTasks ();
         }
 
         // GET api/values/5
-        [HttpGet("{id}", Name = "GetTask")]
-        public IActionResult Get(Guid id)
-        {
-            var foundTask = Repository.GetTaskById(id);
-            if (foundTask == null) return NotFound();
-            
-            return Json(foundTask);
+        [HttpGet ("{id}", Name = "GetTask")]
+        public IActionResult Get (Guid id) {
+            var foundTask = Repository.GetTaskById (id);
+            if (foundTask == null) return NotFound ();
+
+            return Json (foundTask);
         }
 
         // POST api/values
         [HttpPost]
-        public IActionResult Post([FromBody] Models.Task task)
-        {
-            var created = Repository.CreateTask(task);
-            return CreatedAtRoute("GetTask", new {id = created.Id}, created);
+        public IActionResult Post ([FromBody] Models.Task task) {
+            var created = Repository.CreateTask (task);
+            return CreatedAtRoute ("GetTask", new { id = created.Id }, created);
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public IActionResult Put(Guid id, [FromBody] Models.Task task)
-        {
-            var updated = Repository.UpdateTask(id, task);
-            if (updated == null) return NotFound();
-            return Json(updated);
+        [HttpPut ("{id}")]
+        public IActionResult Put (Guid id, [FromBody] Models.Task task) {
+            var updated = Repository.UpdateTask (id, task);
+            if (updated == null) return NotFound ();
+            return Json (updated);
         }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(Guid id)
-        {
-            if (Repository.DeleteTask(id))
-            {
-                return NoContent();
-            }
-
-            return NotFound();
+        [HttpDelete ("{id}")]
+        public IActionResult Delete (Guid id) {
+            if (Repository.DeleteTask (id)) return NoContent ();
+            return NotFound ();
         }
     }
 }
